9354d677bd155f68411339beb29a64e0636ce5da
sourabh0904 <patidarsourabh687@gmail.com>
2022-06-22 00:12:11 +0530

    github

---
+++ github.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    int total=a/5;
+	    if(a%5==0)
+	    {
+	        total--;
+	    }
+	    int unaffected=b/5;
+	    if(b%5==0)
+	    {
+	        unaffected--;
+	    }
+	    cout<<total-unaffected<<endl;
+	}
+	return 0;
+}
